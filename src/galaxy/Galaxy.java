624bba341e15a9c3a8d8e8a55741b9db0fbe980f
AnStar263 <69663294+AnStar263@users.noreply.github.com>
2022-01-19 16:03:42 +0800

    Add files via upload

---
+++ src/galaxy/Galaxy.java
@@ -0,0 +1,95 @@
+package galaxy;
+
+//导入原版包
+import mindustry.world.*;
+import mindustry.entities.*;
+import mindustry.game.*;
+import arc.*;
+import arc.math.*;
+import arc.util.*;
+import arc.graphics.*;
+import arc.graphics.g2d.*;
+import arc.func.*;
+import arc.math.geom.*;
+import arc.scene.ui.layout.*;
+import arc.struct.Bits;
+import arc.struct.*;
+import arc.util.io.*;
+import arc.scene.ui.*;
+import mindustry.core.GameState.*;
+import mindustry.game.EventType.*;
+import mindustry.gen.*;
+import mindustry.ui.*;
+import mindustry.ui.dialogs.*;
+import mindustry.ctype.*;
+import mindustry.content.*;
+import mindustry.world.blocks.defense.turrets.*;
+import mindustry.entities.bullet.*;
+import mindustry.gen.*;
+import mindustry.graphics.*;
+import mindustry.type.*;
+import mindustry.world.*;
+import mindustry.world.blocks.*;
+import mindustry.world.blocks.campaign.*;
+import mindustry.world.blocks.defense.*;
+import mindustry.world.blocks.defense.turrets.*;
+import mindustry.world.blocks.distribution.*;
+import mindustry.world.blocks.environment.*;
+import mindustry.world.blocks.experimental.*;
+import mindustry.world.blocks.legacy.*;
+import mindustry.world.blocks.liquid.*;
+import mindustry.world.blocks.logic.*;
+import mindustry.world.blocks.power.*;
+import mindustry.world.blocks.production.*;
+import mindustry.world.blocks.sandbox.*;
+import mindustry.world.blocks.storage.*;
+import mindustry.world.blocks.units.*;
+import mindustry.world.consumers.*;
+import mindustry.world.draw.*;
+import mindustry.world.meta.*;
+import mindustry.core.*;
+import mindustry.*;
+import arc.*;
+import arc.files.*;
+import arc.util.serialization.*;
+import mindustry.graphics.*;
+import arc.graphics.g2d.*;
+import arc.scene.ui.*;
+import arc.scene.style.TextureRegionDrawable;
+import arc.util.*;
+import mindustry.*;
+import mindustry.content.*;
+import mindustry.game.EventType.*;
+import mindustry.gen.*;
+import mindustry.mod.*;
+import mindustry.ui.Styles;
+import mindustry.ui.dialogs.*;
+
+import java.util.*;
+
+//导入模组内容
+public class Galaxy extends Mod{
+    public Galaxy ()
+	{
+    }
+    
+    @Override
+    public void init(){
+    }
+	 //加载内容
+    @Override
+    public void loadContent()
+	{
+	  /*new QUItems().load();
+	  new QUFx().load();
+	  new QUBullets().load();
+	  new QUTurrets().load();
+	  new QUFactories().load();
+	  new QULargeMachinery().load();
+	  new QULargeTurret().load();
+	  new QUWalls().load();
+	  new QUPlanets().load();
+*/
+    }
+	
+}
