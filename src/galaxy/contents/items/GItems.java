f147f77bc84e123d158be40be7e32e8ddfb9a527
AnStar263 <69663294+AnStar263@users.noreply.github.com>
2022-01-19 16:32:43 +0800

    Add files via upload

---
+++ src/galaxy/contents/items/GItems.java
@@ -0,0 +1,16 @@
+package contents.items;
+import mindustry.content.*;
+import arc.graphics.*;
+import mindustry.ctype.*;
+import mindustry.type.*;
+
+public class GItems implements ContentList{
+    public static Item Iron;
+    public void load(){
+        Iron = new Item("Iron",Color.valueOf("fffaf0")){{
+
+        }};
+
+    }
+
+}
