006e03b6249d1b5808acc27b7383ec1dcfb3c71f
AnStar263 <69663294+AnStar263@users.noreply.github.com>
2022-01-21 18:37:25 +0800

    Update GItems.java

--- src/galaxy/contents/items/GItems.java
+++ src/galaxy/contents/items/GItems.java
@@ -1,13 +1,14 @@
 package galaxy.contents.items;
+import galaxy.contents.*;
 import mindustry.content.*;
 import arc.graphics.*;
 import mindustry.ctype.*;
 import mindustry.type.*;
 
 public class GItems implements ContentList{
-    public static Item Iron;
+    public static GItem Iron;
     public void load(){
-        Iron = new Item("Iron",Color.valueOf("fffaf0")){{
+        Iron = new GItem("Iron"/*,Color.valueOf("fffaf0")*/,20){{
 
         }};
 
