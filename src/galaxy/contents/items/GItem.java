e170967800aabdc8f447d4033ab1d5a828668212
AnStar263 <69663294+AnStar263@users.noreply.github.com>
2022-01-21 18:32:27 +0800

    Update GItem.java

--- src/galaxy/contents/items/GItem.java
+++ src/galaxy/contents/items/GItem.java
@@ -5,7 +5,7 @@ import mindustry.type.*;
 public class GItem extends Item {
   public double newEnergy = 20;
   public GItem(String name,double newEnergy) {
-    super(name,color);
+    super(name);
     this.newEnergy = newEnergy;
     String str = "newEnergy: " + newEnergy;
     description += str;  
