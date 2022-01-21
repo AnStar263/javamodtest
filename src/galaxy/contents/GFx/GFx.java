96066fa193f6c15def1b01a814dd4bae65ac076e
AnStar263 <69663294+AnStar263@users.noreply.github.com>
2022-01-21 21:54:23 +0800

    Update GFx.java

--- src/galaxy/contents/GFx/GFx.java
+++ src/galaxy/contents/GFx/GFx.java
@@ -3,7 +3,12 @@ package galaxy.contents.GFx;
 import mindustry.entities.*;
 import mindustry.ctype.*;
 public class GFx implements ContentList {
-  public static final Effect Radiate = new Effect(99999f,e -> {
+  public Effect Radiate;
+  @override
+  public load() {
+    Radiate = new Effect(99999f,e -> {
     
-  });
+    });
+  }
+  
 }
